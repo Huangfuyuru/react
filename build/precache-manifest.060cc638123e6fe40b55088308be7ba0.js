c4fc7b5335ca2eafbb115236a8a266fa3e428458
Huangfuyuru <https://github.com/Huangfuyuru/JS-advanced-lessons.git>
2019-10-15 08:55:24 +0800

    text

---
+++ build/precache-manifest.060cc638123e6fe40b55088308be7ba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f818b81cf94d8ca3bd0f0ab7c764ea2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "036457ca27f56b51d662",
+    "url": "/static/css/main.185d61ef.chunk.css"
+  },
+  {
+    "revision": "261920dedcd5157ad367",
+    "url": "/static/js/2.acd90a89.chunk.js"
+  },
+  {
+    "revision": "036457ca27f56b51d662",
+    "url": "/static/js/main.e9bbec8c.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
